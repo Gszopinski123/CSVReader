9245b798ef6e5eef517684c2808cee0ebce548f6
Gabriel <gabeszopinski123@gmail.com>
2025-01-22 14:10:57 -0500

    started working on grabbing data from csv and converting it

--- CSVReader.js
+++ CSVReader.js
@@ -1,34 +1,49 @@
 
-
+import * as fs from "fs"
 export class CSVReader {
     csvArray = [[]]
-    constructor(buffer, headers=true) {
-        this.buffer = buffer;
+    buffer = "";
+    constructor(filename, headers=true) {
+        this.filename = filename
         this.headers = headers;
         this.transform()
+        //console.log(this.buffer);
     }
-    transform() {
+    async transform() {
         console.log("Transforming!");
-        this.item = ""
-        this.row = 0
-        for (var i = 0; i != this.buffer.length; ++i) {
-            //new row we need to skip
-            if (this.buffer[i] == '\n') {
-                this.csvArray[this.row].push(this.item);
-                this.csvArray.push([]);
-                this.item = "";
-                this.row++;
+        this.buffer = await fs.readFile(this.filename, (err, data) => {
+            if (err) {
+                console.log(err);
+                console.log("Not passed!");
+            } else {
+                console.log("Passed!");
+                for (var i = 0; i != data.length; i++) {
+                    this.buffer = this.buffer + String.fromCharCode(data[i]);
+                }
             }
-            //end of item need to start a new item
-            else if (this.buffer[i] == ',') {
-                this.csvArray[this.row].push(this.item);
-                this.item = ""
-            }
-            else {
-                this.item += this.buffer[i];
-            }
-        }
-        this.csvArray[this.row].push(this.item);
-        console.log(this.csvArray)
+            console.log(this.buffer);
+            return this.buffer;
+        });
+        // this.item = ""
+        // this.row = 0
+        // for (var i = 0; i != this.buffer.length; ++i) {
+        //     //new row we need to skip
+        //     if (this.buffer[i] == '\n') {
+        //         this.csvArray[this.row].push(this.item);
+        //         this.csvArray.push([]);
+        //         this.item = "";
+        //         this.row++;
+        //     }
+        //     //end of item need to start a new item
+        //     else if (this.buffer[i] == ',') {
+        //         this.csvArray[this.row].push(this.item);
+        //         this.item = ""
+        //     }
+        //     else {
+        //         this.item += this.buffer[i];
+        //     }
+        // }
+        // this.csvArray[this.row].push(this.item);
+        // console.log(this.csvArray)
     }
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //special imports
 import { CSVReader } from './CSVReader.js'
+
 //start of buffer how data should look when importing a file
 var buffer = "row1,row2,row2\nGabriel,Szopinski,21";
-var myReader = new CSVReader(buffer)
+var myReader = new CSVReader("./testing.csv");
 //testing
-console.log(buffer);
